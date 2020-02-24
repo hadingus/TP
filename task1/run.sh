26dd85a39cf43ddd42bf9efb1506fcbbebb711f7
Eugene Tischenko <mr.hadingus@gmail.com>
2020-02-24 23:11:18 +0300

    another sol

--- task1/run.sh
+++ task1/run.sh
@@ -7,9 +7,16 @@ name_arch=$4
 
 dir_path="$(pwd)/$name_dir"
 mkdir $name_dir
-for val in $(find $directory -name "*.$extension")
+for file in $(find $directory -name "*.$extension")
 do
-	cp --parents $val $dir_path
+	name=${file##*/}
+	name=${name%.*}
+	cnt=0
+	for count in $(find $dir_path -name "$name*.$extension")
+	do
+		((cnt++))
+	done
+	cp $file $dir_path/$name\_$cnt.$extension
 done
 tar -acf $name_arch $name_dir
 echo done
